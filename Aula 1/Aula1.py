b09fbbe3e839fb59aa2631d180fc75faa114d6ed
bymell <anabemelloz@gmail.com>
2024-11-28 10:00:46 -0300

    Adicionando arquivos da aula 1

---
+++ Aula 1/Aula1.py
@@ -0,0 +1,4 @@
+print('Olá, mundo')
+print('Minha música do momento é Taste - Sabrina Carpenter')
+print('Comprei uma paleta de sombras com glitter pro ano novo')
+print('Wicked é um filme espetacular, recomendo assistirem!')
