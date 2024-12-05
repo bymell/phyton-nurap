b90df93edfa6f5717295cf76c6b18660d3f74187
bymell <anabemelloz@gmail.com>
2024-12-05 11:25:25 -0300

    Conteúdo Aula 2

---
+++ Aula 2/aula02.py
@@ -0,0 +1,11 @@
+print('Logo teremos festas de natal')
+
+nome_usuario = 'Bibi' # tipo -> str = string 
+idade_usuario = 19 # tipo -> int = integer 
+altura_usuario = 1.76 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print("Seja Bem_vindo", nome_usuario), 
+print("sua idade é" idade_usuario 'anos',)
+print('você tem' altura_usuario 'metros'.)
+print("Você é maior de idade?" maior_idade)
